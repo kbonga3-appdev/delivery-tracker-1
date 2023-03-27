e050bc2069228ebf0d13aa77b3a905da2ce3eb32
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-03-27 02:58:06 +0000

    route sign up

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
   # ------------------------------
 
   # SIGN IN FORM
+  get("/", { :controller => "user_authentication", :action => "sign_in_form" })
   get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
   # AUTHENTICATE AND STORE COOKIE
   post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
